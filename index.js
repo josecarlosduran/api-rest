ba98cf6be8c947ef700546cfd47858c362133c28
Jose Carlos Duran <josecarlosduran@gmail.com>
2022-04-29 14:29:54 +0200

    refactor(general): extract config parameters to a config file

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 
 const app = require('./src/app')
 const mongoose = require('mongoose')
+const config = require('./config')
 
-const port = process.env.PORT || 3000
 
-mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
+mongoose.connect(config.dbUrl, (err, res) => {
     if(err) {
         return console.log(`Error conectando a BD: ${err}`)
     }
     
     console.log ('Conexion a la BD establecida')
-    app.listen(port, () => {
-        console.log(`Api Rest corriendo en http://localhost:${port}`)
+    app.listen(config.port, () => {
+        console.log(`Api Rest corriendo en http://localhost:${config.port}`)
     })
 
 })
