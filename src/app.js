fc940ea02e7ca9828fe739d53a9e8fb494a9cb8b
Jose Carlos Duran <josecarlosduran@gmail.com>
2022-04-29 13:28:25 +0200

    refactor(general): extract express app to new file

---
+++ src/app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const productControllers = require('./controllers/product')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/api/product', (req, res) => productControllers.getAllProducts(req, res))
+app.get('/api/product/:productId', (req, res) => productControllers.getProductById(req,res))
+app.post('/api/product', (req, res) => productControllers.createProduct(req, res))
+app.put('/api/product/:productId', (req, res) => productControllers.updateProduct(req, res))
+app.delete('/api/product/:productId', (req, res) => productControllers.deleteProduct(req, res))
+
+module.exports = app
